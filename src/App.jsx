0a89e42349ab3e7fde054d0c06f3a63116c14736
AnthromaLLC <rui.luo@anthroma.com>
2026-08-01 17:05:33 -0400

    Update App.jsx
    Team info update

--- src/App.jsx
+++ src/App.jsx
@@ -434,13 +434,11 @@ const Home = () => {
 
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
             {[
 
               { name: 'Roy Luo', role: 'Co-Founder / Engineering' },
-
-              { name: 'Bruce Li', role: 'Co-Founder / Product' },
 
               { name: 'Lucas Peng', role: 'Lead Developer' },
 
